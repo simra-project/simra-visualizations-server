21ae775e700ebb15e36351006a7e7b0af950f914
Richard Viehoever (via dev server) <root@vmd54526.contaboserver.net>
2020-09-12 21:58:56 +0200

    turn importer into package

---
+++ importer/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SimRa Importer',
+    version='0.1.0',
+    url='https://github.com/simra-project',
+    author='Richard Viehoever',
+    author_email='richard.viehoever@gmail.com',
+    description='importer for simra',
+    packages=find_packages(),    
+)
