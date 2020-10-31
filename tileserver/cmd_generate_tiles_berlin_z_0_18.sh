ae0b3792b064af652720a11720ec508dfe08186c
Adrian Gronau <adrian.gronau@gmx.de>
2020-10-31 23:47:39 +0100

    Add tile script

---
+++ tileserver/cmd_generate_tiles_berlin_z_0_18.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+tirex-batch --prio=25 map=junctions-stoptimes           bbox=12.587585,52.060935,14.040527,52.855864 z=0-18
+tirex-batch --prio=25 map=rides-density_all             bbox=12.587585,52.060935,14.040527,52.855864 z=0-18
+tirex-batch --prio=25 map=rides-density_rushhourmorning bbox=12.587585,52.060935,14.040527,52.855864 z=0-18
+tirex-batch --prio=25 map=rides-density_rushhourevening bbox=12.587585,52.060935,14.040527,52.855864 z=0-18
+tirex-batch --prio=25 map=rides-density_weekend         bbox=12.587585,52.060935,14.040527,52.855864 z=0-18
+tirex-batch --prio=25 map=rides-original                bbox=12.587585,52.060935,14.040527,52.855864 z=0-18
+tirex-batch --prio=25 map=rides-quality-segments        bbox=12.587585,52.060935,14.040527,52.855864 z=0-18
+tirex-batch --prio=25 map=surface-quality               bbox=12.587585,52.060935,14.040527,52.855864 z=0-18
