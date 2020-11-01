ba350cf23f1f7a07c95c39099d77d1862445297f
Adrian Gronau <adrian.gronau@gmx.de>
2020-11-01 02:40:51 +0100

    Fix mapnik/postgres error

--- tileserver/cmd_generate_tiles_berlin_z_6_15.sh
+++ tileserver/cmd_generate_tiles_berlin_z_6_15.sh
@@ -7,7 +7,6 @@ tirex-batch --prio=25 map=rides-density_rushhourmorning bbox=12.587585,52.060935
 tirex-batch --prio=25 map=rides-density_rushhourevening bbox=12.587585,52.060935,14.040527,52.855864 z=6-15
 tirex-batch --prio=25 map=rides-density_weekend         bbox=12.587585,52.060935,14.040527,52.855864 z=6-15
 tirex-batch --prio=25 map=rides-original                bbox=12.587585,52.060935,14.040527,52.855864 z=6-15
-tirex-batch --prio=25 map=rides-quality-segments        bbox=12.587585,52.060935,14.040527,52.855864 z=6-15
 tirex-batch --prio=25 map=stoptimes                     bbox=12.587585,52.060935,14.040527,52.855864 z=6-15
 tirex-batch --prio=25 map=surface-quality-aggregated    bbox=12.587585,52.060935,14.040527,52.855864 z=6-15
 tirex-batch --prio=25 map=surface-quality-single        bbox=12.587585,52.060935,14.040527,52.855864 z=6-15
