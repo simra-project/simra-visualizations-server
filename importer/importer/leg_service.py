8ad065cd459d7c452fa277a8a2e51ef44bf8ba46
KrokodileDandy <s.fuehr1@gmail.com>
2021-05-27 09:10:55 +0200

    Add documentation and some reformatting

--- importer/importer/leg_service.py
+++ importer/importer/leg_service.py
@@ -1,3 +1,16 @@
+"""Service which manipulates the osmwayslegs database table.
+
+Methods
+-------
+determine_legs
+insert_map_matched_coords
+find_legs
+update_legs
+is_weekday
+is_morning
+is_evening
+"""
+
 import pandas as pd
 
 POSTGIS_SURROUNDING_RIDE_BUFFER_SIZE = 0.00002
@@ -9,21 +22,29 @@ def determine_legs(map_matched_coords, cur):
 
 
 def insert_map_matched_coords(map_matched_coords, cur):
-    cur.execute('DROP TABLE IF EXISTS ride;')
-    cur.execute('CREATE TEMP TABLE ride (geometry geometry NOT NULL);')
+    cur.execute("DROP TABLE IF EXISTS ride;")
+    cur.execute("CREATE TEMP TABLE ride (geometry geometry NOT NULL);")
 
-    insert_map_matched_query = 'INSERT INTO ride VALUES (st_buffer(st_makeline(ARRAY['
+    insert_map_matched_query = "INSERT INTO ride VALUES (st_buffer(st_makeline(ARRAY["
 
     for coord in map_matched_coords:
-        insert_map_matched_query += 'St_SetSRID(st_makepoint(' + str(coord[0]) + ',' + str(coord[1]) + '), 4326),'
+        insert_map_matched_query += (
+            "St_SetSRID(st_makepoint("
+            + str(coord[0])
+            + ","
+            + str(coord[1])
+            + "), 4326),"
+        )
     if len(map_matched_coords) > 0:
         insert_map_matched_query = insert_map_matched_query[:-1]
-    insert_map_matched_query += ']::geometry[]), ' + str(POSTGIS_SURROUNDING_RIDE_BUFFER_SIZE) + '));'
+    insert_map_matched_query += (
+        "]::geometry[]), " + str(POSTGIS_SURROUNDING_RIDE_BUFFER_SIZE) + "));"
+    )
     cur.execute(insert_map_matched_query)
 
 
 def find_legs(cur):
-    query = '''
+    query = """
         SELECT 
            sub.id,
            sub."osmId",
@@ -60,106 +81,183 @@ def find_legs(cur):
              WHERE r.geometry && l.geom
              LIMIT 100000) AS sub, ride r
         WHERE st_contains(r.geometry, sub.geom);
-    '''
+        """
     cur.execute(query)
     return cur.fetchall()
 
 
 def update_legs(ride, legs, cur, IRI, phone_loc, velocity_sections, incident_locs):
-    df = pd.DataFrame(data=legs, index=range(len(legs)),
-                      columns=['id', 'osm_id', 'geometry', 'street_name', 'count', 'score', 'weekday_count',
-                               'morning_count', "evening_count", "score_array", "velocity", "velocity_array", "normal_incident_count", "scary_incident_count"])
-    df['count'] += 1
+    """
+    Parameters
+    ----------
+    phone_loc
+        The location of the phone during the ride.
+    """
+    df = pd.DataFrame(
+        data=legs,
+        index=range(len(legs)),
+        columns=[
+            "id",
+            "osm_id",
+            "geometry",
+            "street_name",
+            "count",
+            "score",
+            "weekday_count",
+            "morning_count",
+            "evening_count",
+            "score_array",
+            "velocity",
+            "velocity_array",
+            "normal_incident_count",
+            "scary_incident_count",
+        ],
+    )
+
+    # Increase counters
+    df["count"] += 1
     for i, leg in df.iterrows():
         if is_weekday(ride.timestamps):
-            df.at[i, 'weekday_count'] += 1
+            df.at[i, "weekday_count"] += 1
         if is_morning(ride.timestamps):
-            df.at[i, 'morning_count'] += 1
+            df.at[i, "morning_count"] += 1
         if is_evening(ride.timestamps):
-            df.at[i, 'evening_count'] += 1
+            df.at[i, "evening_count"] += 1
 
-    cur.execute("""
+    # Create a temporary table to buffer updated leg information
+    cur.execute(
+        """
         CREATE TEMP TABLE updated_legs(count INT, score FLOAT, weekday_count INT, morning_count INT, evening_count INT, id BIGINT, score_array FLOAT[], velocity FLOAT, velocity_array FLOAT[], normal_incident_count INT, scary_incident_count INT) ON COMMIT DROP
-        """)
+        """
+    )
 
-    cur.execute("""
-                CREATE TEMP TABLE legs_to_match(id BIGINT, geom GEOMETRY(LINESTRING,4326)) ON COMMIT DROP
-                """)
-    tup = [tuple(x) for x in df[['id', 'geometry']].to_numpy()]
-    cur.executemany("""
-                    INSERT INTO legs_to_match (id, geom) VALUES(%s, ST_GeomFromText(%s,4326))
-                    """, tup)
+    cur.execute(
+        """
+        CREATE TEMP TABLE legs_to_match(id BIGINT, geom GEOMETRY(LINESTRING,4326)) ON COMMIT DROP
+        """
+    )
+    tup = [tuple(x) for x in df[["id", "geometry"]].to_numpy()]
+    cur.executemany(
+        """
+        INSERT INTO legs_to_match (id, geom) VALUES(%s, ST_GeomFromText(%s,4326))
+        """,
+        tup,
+    )
 
+    # If the phones was attached to the bikes handelbar, calculate a
+    # street quality score.
     if phone_loc == 1 or phone_loc == "1":  # Handlebar
         from tqdm import tqdm
+
         for iri in tqdm(IRI):
-            cur.execute("""
-                        SELECT id, ST_Distance(geom, ST_SetSRID(ST_MakePoint(%s, %s),4326)) as d FROM legs_to_match ORDER BY d ASC LIMIT 1
-                    """, (iri[2][0], iri[2][1]))
+            cur.execute(
+                """
+                SELECT id, ST_Distance(geom, ST_SetSRID(ST_MakePoint(%s, %s),4326)) as d FROM legs_to_match ORDER BY d ASC LIMIT 1
+                """,
+                (iri[2][0], iri[2][1]),
+            )
             candidates = cur.fetchall()
             for candidate in candidates:
                 if candidate[0] in list(df["id"]):
-                    for i in df.loc[df['id'] == candidate[0]]["score_array"]:
+                    for i in df.loc[df["id"] == candidate[0]]["score_array"]:
                         i.append(iri[0])
                     break
 
         for i, leg in df.iterrows():
-            if len(df.at[i, 'score_array']) > 0:
-                df.at[i, 'score'] = sum(df.at[i, "score_array"]) / len(df.at[i, "score_array"])
+            if len(df.at[i, "score_array"]) > 0:
+                df.at[i, "score"] = sum(df.at[i, "score_array"]) / len(
+                    df.at[i, "score_array"]
+                )
             else:
                 df.at[i, "score"] = -1
+
+    # Process near miss incidents
     for incident in incident_locs:
-        cur.execute("""
-                    SELECT id, ST_Distance(geom, %s) as d FROM legs_to_match ORDER BY d ASC LIMIT 1
-        """, (incident[0],))
+        cur.execute(
+            """
+            SELECT id, ST_Distance(geom, %s) as d FROM legs_to_match ORDER BY d ASC LIMIT 1
+            """,
+            (incident[0],),
+        )
         candidates = cur.fetchall()
         for candidate in candidates:
             if candidate[0] in list(df["id"]):
                 if not incident[1]:
-                    df.loc[df['id'] == candidate[0], "normal_incident_count"] += 1
+                    df.loc[df["id"] == candidate[0], "normal_incident_count"] += 1
                 else:
-                    df.loc[df['id'] == candidate[0], "scary_incident_count"] += 1
+                    df.loc[df["id"] == candidate[0], "scary_incident_count"] += 1
 
-
+    # Process velocity data
     for vel_section in velocity_sections:
-        cur.execute("""
-                    SELECT id, ST_Distance(geom, ST_SetSRID(ST_MakePoint(%s, %s),4326)) as d FROM legs_to_match ORDER BY d ASC LIMIT 1
-                """, (vel_section[0][0][0], vel_section[0][0][1]))
+        cur.execute(
+            """
+            SELECT id, ST_Distance(geom, ST_SetSRID(ST_MakePoint(%s, %s),4326)) as d FROM legs_to_match ORDER BY d ASC LIMIT 1
+            """,
+            (vel_section[0][0][0], vel_section[0][0][1]),
+        )
         candidates = cur.fetchall()
         for candidate in candidates:
             if candidate[0] in list(df["id"]):
-                for i in df.loc[df['id'] == candidate[0]]["velocity_array"]:
+                for i in df.loc[df["id"] == candidate[0]]["velocity_array"]:
                     i.append(vel_section[2])
                 break
 
     for i, leg in df.iterrows():
-        if len(df.at[i, 'velocity_array']) > 0:
-            df.at[i, 'velocity'] = sum(df.at[i, "velocity_array"]) / len(df.at[i, "velocity_array"])
+        if len(df.at[i, "velocity_array"]) > 0:
+            df.at[i, "velocity"] = sum(df.at[i, "velocity_array"]) / len(
+                df.at[i, "velocity_array"]
+            )
         else:
             df.at[i, "velocity"] = -1
 
-    tuples = [tuple(x) for x in df[['count', 'score', 'weekday_count', 'morning_count', "evening_count", 'id', 'score_array', "velocity", "velocity_array", "normal_incident_count", "scary_incident_count"]].to_numpy()]
-    cur.executemany("""
-      INSERT INTO updated_legs (count, score, weekday_count, morning_count, evening_count, id, score_array, velocity, velocity_array, normal_incident_count, scary_incident_count)
-      VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
-                    tuples)
+    # Store updated information in the temporary database table
+    # 'updated_legs'.
+    tuples = [
+        tuple(x)
+        for x in df[
+            [
+                "count",
+                "score",
+                "weekday_count",
+                "morning_count",
+                "evening_count",
+                "id",
+                "score_array",
+                "velocity",
+                "velocity_array",
+                "normal_incident_count",
+                "scary_incident_count",
+            ]
+        ].to_numpy()
+    ]
+    cur.executemany(
+        """
+        INSERT INTO updated_legs (count, score, weekday_count, morning_count, evening_count, id, score_array, velocity, velocity_array, normal_incident_count, scary_incident_count)
+        VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """,
+        tuples,
+    )
 
-    cur.execute("""
-            UPDATE public."SimRaAPI_osmwayslegs"
-            SET 
-                count = updated_legs.count,
-                score = updated_legs.score,
-                "weekdayCount" = updated_legs.weekday_count,
-                "morningCount" = updated_legs.morning_count,
-                "eveningCount" = updated_legs.evening_count,
-                "score_array" = updated_legs.score_array,
-                "velocity" = updated_legs.velocity,
-                "velocity_array" = updated_legs.velocity_array,
-                "normalIncidentCount" = updated_legs.normal_incident_count,
-                "scaryIncidentCount" = updated_legs.scary_incident_count
-            FROM updated_legs
-            WHERE updated_legs.id = public."SimRaAPI_osmwayslegs".id;
-            """)
+    # Move updated leg data into the persistend database table
+    # 'SimRaAPI_osmwayslegs'.
+    cur.execute(
+        """
+        UPDATE public."SimRaAPI_osmwayslegs"
+        SET 
+            count = updated_legs.count,
+            score = updated_legs.score,
+            "weekdayCount" = updated_legs.weekday_count,
+            "morningCount" = updated_legs.morning_count,
+            "eveningCount" = updated_legs.evening_count,
+            "score_array" = updated_legs.score_array,
+            "velocity" = updated_legs.velocity,
+            "velocity_array" = updated_legs.velocity_array,
+            "normalIncidentCount" = updated_legs.normal_incident_count,
+            "scaryIncidentCount" = updated_legs.scary_incident_count
+        FROM updated_legs
+        WHERE updated_legs.id = public."SimRaAPI_osmwayslegs".id;
+        """
+    )
 
 
 def is_weekday(timestamps):
@@ -167,11 +265,10 @@ def is_weekday(timestamps):
 
 
 def is_morning(timestamps):
-    hour = timestamps[int(len(timestamps)/2)].hour
+    hour = timestamps[int(len(timestamps) / 2)].hour
     return 6 <= hour < 9
 
 
 def is_evening(timestamps):
-    hour = timestamps[int(len(timestamps)/2)].hour
+    hour = timestamps[int(len(timestamps) / 2)].hour
     return 16 <= hour < 19
-
