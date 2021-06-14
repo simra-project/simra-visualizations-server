a3ebfba70b639c7ce15fcb6d7644fe3a1242e9e9
KrokodileDandy <s.fuehr1@gmail.com>
2021-06-14 07:14:20 +0200

    Add a prototype for a popularity combined map

--- tileserver/cmd_generate_tiles_berlin_z_0_18.sh
+++ tileserver/cmd_generate_tiles_berlin_z_0_18.sh
@@ -10,3 +10,4 @@ tirex-batch --prio=25 map=rides-original                bbox=12.587585,52.060935
 tirex-batch --prio=25 map=stoptimes                     bbox=12.587585,52.060935,14.040527,52.855864 z=0-18
 tirex-batch --prio=25 map=surface-quality-aggregated    bbox=12.587585,52.060935,14.040527,52.855864 z=0-18
 tirex-batch --prio=25 map=surface-quality-single        bbox=12.587585,52.060935,14.040527,52.855864 z=0-18
+tirex-batch --prio=25 map=popularity-combined           bbox=12.587585,52.060935,14.040527,52.855864 z=0-18
